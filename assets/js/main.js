bcd2b5e6afd29e92965afca41ff18dc1ef087302
Lucas Class <lucas.azevedo@instacarro.com>
2022-04-11 00:42:35 -0300

    tabs and javascript

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContens = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContens.forEach(tc => {/*tc = tabcontent*/
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+
+        tabs.forEach(t => {
+            t.classList.remove('filter-tab-active')
+        })
+    })
+})
