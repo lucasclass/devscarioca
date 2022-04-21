33e6cac74109efb554c8c638ec75e199b23f9772
Lucas Class <lucas.azevedo@instacarro.com>
2022-04-21 14:26:40 -0300

    reveal and new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,12 @@ tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
         const target = document.querySelector(tab.dataset.target)
 
-        tabContens.forEach(tc => {/*tc = tabcontent*/
+        tabContens.forEach(tc =>{
             tc.classList.remove('filters__active')
         })
         target.classList.add('filters__active')
 
-        tabs.forEach(t => {
+        tabs.forEach(t =>{
             t.classList.remove('filter-tab-active')
         })
         tab.classList.add('filter-tab-active')
@@ -46,3 +46,19 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+const sr = ScrollReveal({
+    origin : 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.profile__border')
+sr.reveal('.profile__name', {delay:500})
+sr.reveal('.profile__profession', {delay:600})
+sr.reveal('.profile__social', {delay:700})
+sr.reveal('.profile__info-group', {interval:100, delay:700})
+sr.reveal('.profile__buttons', {delay:800})
+sr.reveal('.filters__content', {delay:900})
+sr.reveal('.filters', {delay:1000})
